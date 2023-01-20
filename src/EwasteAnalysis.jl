064890f1f255f9da3724c48fa272e63debd7399e
unknown <sanjay.dev.x@gmail.com>
2023-01-20 20:26:30 +0530

    Loads data into data frame

--- src/EwasteAnalysis.jl
+++ src/EwasteAnalysis.jl
@@ -5,5 +5,6 @@ using BenchmarkTools
 using Statistics
 
 H = readdlm("C:\\Users\\Dell\\Downloads\\Trade.csv",',',header=true);
-Ca = CSV.read("Data.csv",DataFrame, header = true);
+Ca = CSV.read("C:\\Users\\Dell\\Downloads\\Data.csv",DataFrame, header = true);
 C=dropmissing(Ca)
+println(C)
